918bd0908da0e6f52a7279a7a8ebf50e39cfdb4c
Code Plus Coffee <m416a@protonmail.com>
2020-11-24 12:28:55 +0530

    Update placeholder text.

--- src/data/placeholder.js
+++ src/data/placeholder.js
@@ -18,20 +18,20 @@ export const placeholderText = `# 👋, Welcome to Marked Preview
 
 ## Get instant preview of the markdown you type in the editor. ⚡
 
-That being said, [Undesign](https://undesign.learn.uno/) is a place where you can find intriguing developer resources.
+That being said, [Undesign](https://undesign.learn.uno) is a place where you can find intriguing developer resources.
 
 How about getting a random value from an array, arr?
 \`Math.floor(Math.random() * arr.length)\`
 
 🖥️ **PC Shopping List**
-1. [ ] Intel Core i5 10600K
-2. [ ] Asus ROG Strix Z490F
-3. [x] G.Skill TridentZ 32GB DDR4 @ 3200MHz
+1. [ ] AMD Ryzen 7 5800X
+2. [ ] Asus ROG Strix B550-F Gaming
+3. [x] G.Skill TridentZ 32GB DDR4 @ 3600MHz
 4. [x] Samsung 970 Pro, 1TB NVMe PCIe SSD
 5. [x] Seagate Firecuda 2TB SSHD
 6. [ ] MSI | Nvidia RTX 2070 Super
 7. [x] NZXT H510 Elite
-8. [x] Corsair RM750
+8. [x] Corsair RM850
 
 Here is a random quote for you -
 > ${randomQuote.quote}
@@ -39,16 +39,16 @@ Here is a random quote for you -
 
 \`\`\`javascript
 const Say = ({ message }) => (
-    <React.Fragment>
+    <>
         <h1>{message}</h1>
-    </React.Fragment>
+    </>
 )
 \`\`\`
 
 An image that can be your next wallpaper. 📷
-![Image by Paweł Czerwiński on Unsplash](https://images.unsplash.com/photo-1590508965885-90c5502bd6f4?auto=format&fit=crop&w=1050&q=80)
+![Random Image from Unsplash](https://source.unsplash.com/1920x1080/?textures-patterns)
 
-You can take a look at the source code over at [Github](https://github.com/code-plus-coffee/marked-preview) or maybe, play around with the code on [Codesandbox](https://codesandbox.io/s/1ekvy).
+You can take a look at the source code over at [Github](https://github.com/kasipavankumar/marked-preview) or maybe, play around with the code on [Codesandbox](https://codesandbox.io/s/1ekvy).
 
 **Code Plus Coffee • ${currentYear}**
 `
